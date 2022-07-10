91a9c3ef716556d6f11268fdc6354681324828a3
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2022-07-09 23:57:53 -0300

    Clean startup container

--- src/Containers/StartupContainer.tsx
+++ src/Containers/StartupContainer.tsx
@@ -1,44 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import auth from '@react-native-firebase/auth';
+import React, { useEffect } from 'react';
 import { ActivityIndicator, View, Text } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '@/Hooks';
 import { Brand } from '@/Components';
 import { setDefaultTheme } from '@/Store/Theme';
-import { navigate } from '@/Navigators/utils';
 
 const StartupContainer = () => {
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
   const { Layout, Gutters, Fonts } = useTheme();
 
   const { t } = useTranslation();
 
-  const onAuthStateChanged = (user: any) => {
-    console.log('onAuthStateChanged', user);
-    setUser(user);
-    if (initializing) {
-      setInitializing(false);
-    }
-    init();
-  };
-
-  const init = async () => {
-    if (initializing) {
-      return null;
-    }
-    if (!user) {
-      navigate('Main', { screen: 'Login' });
-    } else {
-      navigate('Main', { screen: 'Home' });
-    }
-
+  const init = () => {
     setDefaultTheme({ theme: 'default', darkMode: false });
   };
 
   useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
+    init();
   });
 
   return (
