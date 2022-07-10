26ce0495950f0fc0c1403b817c40c97e4c1d068f
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2022-07-09 23:58:28 -0300

    Add user service

---
+++ src/Services/modules/auth/index.ts
@@ -0,0 +1,53 @@
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Config from 'react-native-config';
+
+GoogleSignin.configure({
+  webClientId: Config.FIREBASE_WEBCLIENT_ID,
+});
+
+const AuthService = class AuthService {
+  signInWithGoogle(): Promise<FirebaseAuthTypes.UserCredential> {
+    return GoogleSignin.signIn().then(({ idToken }) => {
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      return auth().signInWithCredential(googleCredential);
+    });
+  }
+
+  signUpWithEmailAndPassword(
+    email: string,
+    password: string
+  ): Promise<FirebaseAuthTypes.UserCredential | void> {
+    return auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => {
+        if (error.code === 'auth/email-already-in-use') {
+          throw new Error('Email already in use');
+        }
+        if (error.code === 'auth/invalid-email') {
+          throw new Error('That email address is invalid!');
+        }
+      });
+  }
+
+  signInWithEmailAndPassword(
+    email: string,
+    password: string
+  ): Promise<FirebaseAuthTypes.UserCredential> {
+    return auth().signInWithEmailAndPassword(email, password);
+  }
+
+  signOut(): Promise<void> {
+    return auth().signOut();
+  }
+
+  getCurrentUser(): FirebaseAuthTypes.User | null {
+    return auth().currentUser;
+  }
+
+  sendPasswordResetEmail(email: string): Promise<void> {
+    return auth().sendPasswordResetEmail(email);
+  }
+};
+
+export default new AuthService();
