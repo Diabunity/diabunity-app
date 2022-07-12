9fb4e572b604819e72d8a98b56253628f6d4c5c7
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2022-07-12 18:08:33 -0300

    Extract FormButton as a Component

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,2 +1,3 @@
 export { default as Brand } from './Brand';
 export { default as NfcPromptAndroid } from './NfcPromptAndroid';
+export { default as FormButton } from './FormButton';
--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -2,4 +2,3 @@ export { default as ExampleContainer } from './HomeContainer';
 export { default as StartupContainer } from './SplashContainer';
 export { default as AuthContainer } from './AuthContainer';
 export { default as UserContainer } from './UserContainer';
-
