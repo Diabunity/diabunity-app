331280e74358036dd627e7626e942e5ad094e9d7
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2022-07-13 20:47:04 -0300

    Update exports

--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -1,4 +1,5 @@
 export { default as ExampleContainer } from './HomeContainer';
 export { default as StartupContainer } from './SplashContainer';
-export { default as AuthContainer } from './AuthContainer';
+export { default as SignInContainer } from './SignInContainer';
+export { default as SignUpContainer } from './SignUpContainer';
 export { default as UserContainer } from './UserContainer';
