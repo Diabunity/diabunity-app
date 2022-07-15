089ea304be4862bbf884d7defbf7b29c1d3adecc
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2022-07-15 20:25:30 -0300

    Add styles

---
+++ src/Containers/ForgotPasswordContainer/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  text: {
+    width: 283,
+    paddingBottom: 22,
+  },
+  textField: {
+    width: 277,
+    height: 52,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    backgroundColor: 'rgba(0, 0, 0, 0.06)',
+    borderTopRightRadius: 4,
+    borderTopLeftRadius: 4,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgba(0, 0, 0, 0.42)',
+  },
+});
