675d6fdb5b876d681035e04e18ee206db9e5b648
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2022-07-15 20:25:39 -0300

    Export container

--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -2,3 +2,4 @@ export { default as ExampleContainer } from './HomeContainer';
 export { default as StartupContainer } from './SplashContainer';
 export { default as AuthContainer } from './AuthContainer';
 export { default as UserContainer } from './UserContainer';
+export { default as ForgotPasswordContainer } from './ForgotPasswordContainer';
