8e672d13e7a0d3aa109a8bc45f309cb028376242
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2022-07-14 23:39:06 -0300

    Export AuthContainer instead

--- src/Containers/index.ts
+++ src/Containers/index.ts
@@ -1,5 +1,4 @@
 export { default as ExampleContainer } from './HomeContainer';
 export { default as StartupContainer } from './SplashContainer';
-export { default as SignInContainer } from './SignInContainer';
-export { default as SignUpContainer } from './SignUpContainer';
+export { default as AuthContainer } from './AuthContainer';
 export { default as UserContainer } from './UserContainer';
