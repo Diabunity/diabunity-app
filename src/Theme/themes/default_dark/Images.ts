bfbc0d11dc22c90a4534ec2aedd6947bb17d4138
Mati Dastugue <mdastugu@amazon.com>
2022-09-04 16:21:31 -0300

    Remove dark theme for Android since it is not implemented

--- src/Theme/themes/default_dark/Images.ts
+++ src/Theme/themes/default_dark/Images.ts
@@ -3,10 +3,5 @@
  */
 
 export default function () {
-  return {
-    logo: {
-      uri:
-        'https://thecodingmachine.github.io/react-native-boilerplate/img/TOM-small.png',
-    },
-  };
+  return {};
 }
