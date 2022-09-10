7e76a090c95fa57daf5cc7244e4c3b46349d937f
Mati Dastugue <mdastugu@amazon.com>
2022-09-09 23:01:22 -0300

    Add medical reports

--- src/Utils/index.tsx
+++ src/Utils/index.tsx
@@ -147,3 +147,17 @@ export const getNameInitials = (
   }
   return initials;
 };
+
+export const format = (inputDate: Date) => {
+  let date, month, year;
+
+  date = inputDate.getDate();
+  month = inputDate.getMonth() + 1;
+  year = inputDate.getFullYear();
+
+  date = date.toString().padStart(2, '0');
+
+  month = month.toString().padStart(2, '0');
+
+  return `${date}/${month}/${year}`;
+};
