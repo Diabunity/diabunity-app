4accf0b889f7bb91cc74d8a9c4220b304e8ff2c9
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2022-10-13 22:11:37 -0300

    Ranking working with mock data. Still need some refactors.

--- src/Containers/UserContainer/Ranking.tsx
+++ src/Containers/UserContainer/Ranking.tsx
@@ -5,54 +5,70 @@ import { useTheme } from '@/Hooks';
 import { getNameInitials } from '@/Utils';
 import { generateTableStyles } from '../HistoryContainer/styles';
 
-const currentPosition = 3;
-const data = [
-  {
-    username: 'Miguel Angel',
-    picture: 'https://randomuser.me/api/portraits/med/men/75.jpg',
-    percentage: 100,
+interface User {
+  username: string;
+  picture?: string;
+  percentage: number;
+  position?: number;
+}
+
+const data = {
+  ranking: [
+    {
+      username: 'Miguel Angel',
+      picture: 'https://randomuser.me/api/portraits/med/men/75.jpg',
+      percentage: 100,
+    },
+    {
+      username: 'Ariel Gonzalez',
+      picture: 'https://randomuser.me/api/portraits/med/men/74.jpg',
+      percentage: 90,
+    },
+    {
+      username: 'Juan Carlos',
+      picture: 'https://randomuser.me/api/portraits/med/women/75.jpg',
+      percentage: 80,
+    },
+    {
+      username: 'Miguel Angel',
+      picture: 'https://randomuser.me/api/portraits/med/men/76.jpg',
+      percentage: 79,
+    },
+    {
+      username: 'Ariel Gonzalez',
+      picture: 'https://randomuser.me/api/portraits/med/men/77.jpg',
+      percentage: 78,
+    },
+    {
+      username: 'Juan Carlos',
+      picture: 'https://randomuser.me/api/portraits/med/20.jpg',
+      percentage: 77,
+    },
+    {
+      username: 'Ariel Gonzalez',
+      picture: 'https://randomuser.me/api/portraits/med/men/77.jpg',
+      percentage: 76,
+    },
+    {
+      username: 'Juan Carlos',
+      picture: 'https://randomuser.me/api/portraits/med/20.jpg',
+      percentage: 75,
+    },
+  ],
+  user_info: {
+    position: 5,
   },
-  {
-    username: 'Ariel Gonzalez',
-    picture: 'https://randomuser.me/api/portraits/med/men/74.jpg',
-    percentage: 90,
-  },
-  {
-    username: 'Juan Carlos',
-    picture: 'https://randomuser.me/api/portraits/med/women/75.jpg',
-    percentage: 80,
-  },
-  {
-    username: 'Miguel Angel',
-    picture: 'https://randomuser.me/api/portraits/med/men/76.jpg',
-    percentage: 79,
-  },
-  {
-    username: 'Ariel Gonzalez',
-    picture: 'https://randomuser.me/api/portraits/med/men/77.jpg',
-    percentage: 78,
-  },
-  {
-    username: 'Juan Carlos',
-    picture: 'https://randomuser.me/api/portraits/med/20.jpg',
-    percentage: 77,
-  },
-  {
-    username: 'Ariel Gonzalez',
-    picture: 'https://randomuser.me/api/portraits/med/men/77.jpg',
-    percentage: 76,
-  },
-  {
-    username: 'Juan Carlos',
-    picture: 'https://randomuser.me/api/portraits/med/20.jpg',
-    percentage: 75,
-  },
-];
+};
 
 const Ranking = () => {
   const { Colors } = useTheme();
   const styles = generateTableStyles(Colors);
 
+  const userPosition = data.user_info.position;
+  const userData: User = {
+    ...data.ranking[userPosition - 1],
+    position: userPosition,
+  };
   return (
     <>
       <Text
@@ -66,12 +82,87 @@ const Ranking = () => {
       >
         Ranking mensual
       </Text>
+      <View
+        style={{
+          marginHorizontal: 21,
+          shadowColor: '#000',
+          shadowOffset: {
+            width: 2,
+            height: 2,
+          },
+          shadowOpacity: 0.2,
+          shadowRadius: 2,
+          elevation: 2,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: '700',
+            alignSelf: 'center',
+            paddingTop: 10,
+          }}
+        >
+          {userData.username}
+        </Text>
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+          }}
+        >
+          <View>
+            <Text style={{ fontSize: 14, color: 'rgba(0, 0, 0, 0.87)' }}>
+              Puesto
+            </Text>
+            <Text
+              style={{
+                textAlign: 'center',
+                color: Colors.red,
+                fontWeight: '900',
+              }}
+            >
+              {userData.position}
+            </Text>
+          </View>
+          <Avatar
+            size={48}
+            containerStyle={{
+              marginTop: 6,
+              marginBottom: 14,
+              marginHorizontal: 47,
+            }}
+            animate
+            isOnline
+            imageProps={{ animationDuration: 1000 }}
+            labelColor={Colors.white}
+            backgroundColor={Colors.red}
+            source={{ uri: userData.picture }}
+            label={getNameInitials(userData.username)}
+          />
+          <View>
+            <Text style={{ fontSize: 14, color: 'rgba(0, 0, 0, 0.87)' }}>
+              Objetivo
+            </Text>
+            <Text
+              style={{
+                textAlign: 'center',
+                color: Colors.red,
+                fontWeight: '900',
+              }}
+            >
+              {userData.percentage}%
+            </Text>
+          </View>
+        </View>
+      </View>
       <ScrollView
         style={{ width: '100%' }}
         contentContainerStyle={{ paddingBottom: 70 }}
       >
-        {data.map((item, index) => {
-          const isUserPosition = index === currentPosition - 1;
+        {data.ranking.map((item, index) => {
+          const isUserPosition = index === userPosition - 1;
 
           return (
             <View
