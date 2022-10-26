00ef2e234c12e05d69618744462a9475be2fc41f
Mati Dastugue <mdastugu@amazon.com>
2022-10-26 14:02:29 -0300

    Fix merge

--- src/Containers/UserContainer/Ranking.tsx
+++ src/Containers/UserContainer/Ranking.tsx
@@ -7,7 +7,7 @@ import { userApi } from '@/Services/modules/users';
 import { rankingStyles } from './styles';
 import { FirebaseAuthTypes } from '@react-native-firebase/auth';
 
-const Ranking = ({ user }: { user: FirebaseAuthTypes.User }) => {
+const Ranking = ({ user }: { user: FirebaseAuthTypes.User | null }) => {
   const { Colors } = useTheme();
   const { data, isFetching } = userApi.useFetchRankingQuery();
   const userPosition: number | null = data?.user_info?.position ?? null;
@@ -16,7 +16,12 @@ const Ranking = ({ user }: { user: FirebaseAuthTypes.User }) => {
         ...data?.ranking[userPosition],
         position: userPosition + 1,
       }
-    : { username: user.displayName, picture: user.photoURL, position: null };
+    : {
+        username: user?.displayName,
+        picture: user?.photoURL,
+        position: null,
+        percentage: null,
+      };
 
   return (
     <>
