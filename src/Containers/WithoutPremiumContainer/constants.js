01d8a8f73018a95887cc4b5b70a2d4ad1841e1dc
Mati [C] Dastugue <mdastugu@amazon.com>
2023-01-11 12:33:47 -0300

    Add premium container + logos

---
+++ src/Containers/WithoutPremiumContainer/constants.js
@@ -0,0 +1,25 @@
+export const premiumFeatures = [
+  {
+    id: 1,
+    title: 'Mediciones ilimitadas',
+    description:
+      'Mediciones continuas sin limite diario para mantenerte controlado.',
+  },
+  {
+    id: 2,
+    title: 'Reportes médicos',
+    description:
+      'Generar PDF con tus estadísticas para mostrarselas al personal médico.',
+  },
+  {
+    id: 3,
+    title: 'Notificaciones',
+    description:
+      'Alertas de aviso de mediciones para no perder la continuidad.',
+  },
+  {
+    id: 4,
+    title: 'Historial extendido',
+    description: 'Visualización de mas de una semana de tus mediciones.',
+  },
+];
---
+++ src/Containers/WithoutPremiumContainer/styles.ts
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from 'react-native-ui-lib';
+
+export const styles = StyleSheet.create({
+  proImage: {
+    height: 70,
+    width: 350,
+  },
+  header: {
+    justifyContent: 'space-between',
+  },
+  container: {
+    backgroundColor: Colors.white,
+    padding: 20,
+    paddingBottom: 10,
+    marginHorizontal: 20,
+    borderRadius: 3,
+  },
+  rowItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  backgroundImage: {
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: '900',
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: '400',
+  },
+  featureTitle: {
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+
+  dropShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+});
