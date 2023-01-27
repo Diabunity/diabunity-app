05959f2b1aed2caa1fc2414d7f5bd23bedfd7087
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2023-01-26 22:00:50 -0300

    Add tons of things that doesn't work at all

--- src/Services/modules/auth/index.ts
+++ src/Services/modules/auth/index.ts
@@ -1,17 +1,22 @@
 import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import Config from 'react-native-config';
+import analytics from '@react-native-firebase/analytics';
 
 GoogleSignin.configure({
   webClientId: Config.FIREBASE_WEBCLIENT_ID,
 });
 
 const AuthService = class AuthService {
-  signInWithGoogle(): Promise<FirebaseAuthTypes.UserCredential> {
-    return GoogleSignin.signIn().then(({ idToken }) => {
-      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-      return auth().signInWithCredential(googleCredential);
-    });
+  signInWithGoogle(): Promise<FirebaseAuthTypes.UserCredential | void> {
+    return GoogleSignin.signIn()
+      .then(({ idToken }) => {
+        const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+        return auth().signInWithCredential(googleCredential);
+      })
+      .catch(async (error) => {
+        await analytics().logEvent('sign_in_with_google_error', error);
+      });
   }
 
   signUpWithEmailAndPassword(
@@ -20,7 +25,11 @@ const AuthService = class AuthService {
   ): Promise<FirebaseAuthTypes.UserCredential | void> {
     return auth()
       .createUserWithEmailAndPassword(email, password)
-      .catch((error) => {
+      .catch(async (error) => {
+        await analytics().logEvent(
+          'sign_up_with_email_and_password_error',
+          error
+        );
         if (error.code === 'auth/email-already-in-use') {
           throw new Error('El correo eléctronico está en uso.');
         }
