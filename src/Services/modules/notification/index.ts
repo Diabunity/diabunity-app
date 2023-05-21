e7080598472733f5ab43230701517ddee3ab4ce9
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2023-05-21 15:17:34 -0300

    Notification updates to handle quit state

--- src/Services/modules/notification/index.ts
+++ src/Services/modules/notification/index.ts
@@ -1,4 +1,50 @@
+import { VIEW_NAMES } from '@/Constants/views';
+import { FirebaseResponseData, ParsedObject } from '@/index';
 import notifee, { AndroidImportance, EventType } from '@notifee/react-native';
+import { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
+
+const handleNotification = (notificationData: ParsedObject) => {
+  const { type } = notificationData;
+
+  if (type === 'navigate') {
+    const { goTo, navigationRef } = notificationData;
+    const hasValidGoTo = goTo ? Object.keys(VIEW_NAMES).includes(goTo) : null;
+
+    if (hasValidGoTo) {
+      const viewName = VIEW_NAMES[goTo as keyof typeof VIEW_NAMES];
+
+      // TODO: send data to view
+      // const viewData = notificationData.viewData;
+      navigationRef.current?.navigate(viewName);
+    }
+  }
+};
+
+const parseResponse = (response: FirebaseResponseData): ParsedObject | null => {
+  const type = response.type;
+
+  if (type === 'navigate') {
+    if (response.go_to) {
+      const { go_to: goTo } = response;
+
+      return {
+        type,
+        goTo,
+      };
+    }
+  } else if (type === 'static') {
+    if (response.staticPropExample) {
+      const { staticPropExample } = response;
+
+      return {
+        type,
+        staticProp: staticPropExample,
+      };
+    }
+  }
+
+  return null; // Return null if the type is unknown or properties are missing
+};
 
 export enum NotificationState {
   FOREGROUND = 'foreground',
@@ -13,7 +59,10 @@ const Notification = class Notification {
     importance: AndroidImportance.DEFAULT,
   };
 
-  handleMessageReceived = async (message: any, type: NotificationState) => {
+  handleMessageReceived = async (
+    message: FirebaseMessagingTypes.RemoteMessage,
+    type: NotificationState
+  ) => {
     // Request permissions (required for iOS)
     await notifee.requestPermission();
 
@@ -28,8 +77,8 @@ const Notification = class Notification {
 
     // Display a notification
     const notificationId = await notifee.displayNotification({
-      title: message.notification.title,
-      body: message.notification.body,
+      title: message?.notification?.title,
+      body: message?.notification?.body,
       android: {
         channelId,
       },
@@ -53,15 +102,32 @@ const Notification = class Notification {
     });
   };
 
-  handleBackgroundEvent = async (message: any, notificationId: string) => {
+  handleBackgroundEvent = async (
+    message: FirebaseMessagingTypes.RemoteMessage,
+    notificationId: string
+  ) => {
     console.log('Background notification message:', message);
     // Remove the notification
     await notifee.cancelNotification(notificationId);
   };
 
-  handleQuitEvent = async (remoteMessage: any) => {
+  handleQuitEvent = async (
+    remoteMessage: FirebaseMessagingTypes.RemoteMessage
+  ) => {
     console.log('Message handled in quit state!', remoteMessage);
-    // TODO: Logic to store the message in the storage for later use
+    global.notificationData = parseResponse(
+      remoteMessage.data as unknown as FirebaseResponseData
+    );
+  };
+
+  checkForNotification = (navigationRef: any) => {
+    const { notificationData } = global;
+    if (notificationData) {
+      notificationData.navigationRef = navigationRef;
+      handleNotification(notificationData);
+    }
+
+    global.notificationData = null;
   };
 };
 
