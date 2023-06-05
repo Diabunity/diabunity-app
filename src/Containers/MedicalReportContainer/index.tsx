fa8b4c2711fd98d10abd87c399b4f18c38056741
Mati Dastugue <matiasdastugue@gmail.com>
2023-06-04 21:42:22 -0300

    [Reports] Integrate reports feature with BE (#51)
    * Integrate reports feature with the backend
    
    * Update feedback

--- src/Containers/MedicalReportContainer/index.tsx
+++ src/Containers/MedicalReportContainer/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import moment from 'moment';
-import { View, Platform, Image } from 'react-native';
+import { View, Platform, Image, ActivityIndicator } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
+import analytics from '@react-native-firebase/analytics';
 import { WebView } from 'react-native-webview';
 import RNHTMLtoPDF from 'react-native-html-to-pdf';
 import FileViewer from 'react-native-file-viewer';
@@ -10,6 +11,8 @@ import { Incubator } from 'react-native-ui-lib';
 import Handlebars from 'react-native-handlebars/dist/handlebars';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { NavigatorParams } from '@/Navigators/Application';
+import AuthService from '@/Services/modules/auth';
+import { userApi } from '@/Services/modules/users';
 import { useTheme } from '@/Hooks';
 import { store } from '@/Store';
 import { setNotification } from '@/Store/Notification';
@@ -17,9 +20,29 @@ import { BackButton } from '@/Components';
 import { DatePeriod } from '@/Utils';
 // @ts-ignore
 import reportTemplate from '@/Templates/report.html';
-import mockReportData from '@/Mocks/reports';
+
 import { ANDROID_REPORT_WEBVIEW_DATA, base64Logo } from '@/Constants';
 import { handleReportData } from '@/Utils/reports';
+import { Colors } from '@/Theme/Variables';
+
+// Custom helper: lookup
+Handlebars.registerHelper(
+  'lookup',
+  function (array: unknown[], index: number): unknown {
+    if (Array.isArray(array) && index >= 0 && index < array.length) {
+      return array[index];
+    }
+  }
+);
+
+// Custom helper: isOdd
+Handlebars.registerHelper('isOdd', function (index: number): boolean {
+  if (index === 0) {
+    return false;
+  }
+
+  return index % 2 === 0;
+});
 
 // Custom helper: isSame
 Handlebars.registerHelper('isSame', function (a: any, b: any): boolean {
@@ -54,14 +77,50 @@ const getInjectableJSMessage = (message: unknown) =>
     })();
   `;
 
+const Spinner = () => {
+  const { Layout } = useTheme();
+  return (
+    <View style={[Layout.fill, Layout.center]}>
+      <ActivityIndicator size="small" color={Colors.red} />
+    </View>
+  );
+};
+
 const MedicalReportContainer = ({
   route,
   navigation: { goBack, canGoBack },
 }: Props) => {
   const { Layout, Images } = useTheme();
-  const reportData = mockReportData;
+  const user = AuthService.getCurrentUser();
   const webViewRef = React.useRef(null);
   const { filter } = route?.params || { filter: DatePeriod.LAST_DAY };
+  const {
+    data: reportData,
+    isFetching,
+    error,
+  } = userApi.useFetchReportQuery({
+    id: user?.uid,
+    dateFilter: filter,
+  });
+
+  const handleError = (errMessage: string) => {
+    store.dispatch(
+      setNotification({
+        preset: Incubator.ToastPresets.FAILURE,
+        message: errMessage,
+      })
+    );
+    if (canGoBack()) {
+      goBack();
+    }
+  };
+
+  useEffect(() => {
+    if (error) {
+      handleError('Hubo un error al obtener los datos del reporte.');
+      analytics().logEvent('error_generating_report', { error });
+    }
+  }, [error]);
   const webViewSource =
     Platform.OS === 'ios'
       ? require('@/Templates/webviews/charts.html')
@@ -71,16 +130,15 @@ const MedicalReportContainer = ({
     const today = moment(new Date());
     const processedData = handleReportData(reportData, filter);
     const template = Handlebars.compile(reportTemplate);
-
     const options = {
       html: template({
         rangeChartData: data.images.range,
         lineChartData: data.images.line,
-        measurementTimestamps:
-          processedData.user_info.measurements_info.results.map(
-            (r: any) => r.timestamp
-          ),
+        measurementTimestamps: processedData.measurements_info.results.map(
+          (r: any) => r.timestamp
+        ),
         user_info: processedData.user_info,
+        measurements_info: processedData.measurements_info,
         logo: base64Logo,
         type: processedData.type[1],
         title: `Reporte ${processedData.type[0]}: ${processedData.dateRange}`,
@@ -113,10 +171,19 @@ const MedicalReportContainer = ({
     await handleCapture(data);
   };
   const sendDataToWebView = () => {
+    if (!reportData) {
+      handleError('No hay datos para generar el reporte.');
+      analytics().logEvent('error_generating_report', { error: 'no_data' });
+      return;
+    }
     const processedData = handleReportData(reportData, filter);
     //@ts-ignore
     webViewRef.current?.injectJavaScript(getInjectableJSMessage(processedData));
   };
+
+  if (isFetching) {
+    return <Spinner />;
+  }
 
   return (
     <View style={[Layout.fill]}>
@@ -131,6 +198,7 @@ const MedicalReportContainer = ({
           onLoadEnd={sendDataToWebView}
           bounces={false}
           startInLoadingState
+          renderLoading={() => <Spinner />}
           originWhitelist={['*']}
           onMessage={onMessageReceived}
           domStorageEnabled={true}
